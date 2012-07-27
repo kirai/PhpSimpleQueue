e25b3d5c7da4d758a4acef6c7f6e11509d69e5b0
Hector Garcia <hectorgarcia@gmail.com>
2012-07-27 16:17:16 +0900

    Added supervisor to check how many tasks are waiting in the queue! !

---
+++ supervisor.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'myQueue.php';
+require 'config.php';
+
+$pdo = new PDO('mysql:host='.HOST.';dbname='.DB_NAME.';', DB_USER, DB_PASSW);
+
+while(1) {
+  $result = $pdo->prepare('SELECT count(*) FROM myqueue');
+  $result->execute(); 
+  $number_of_rows = $result->fetchColumn();
+  echo "Number of tasks waiting in the queue: ".$number_of_rows;
+  echo "\r\n";
+  sleep(5);
+}
+
